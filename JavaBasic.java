82783890b0b339489cf5e32095514263bb0d6748
OM PRAKASH <og04914@gmail.com>
2026-07-31 19:04:55 +0530

    Basic

---
+++ JavaBasic.java
@@ -0,0 +1,15 @@
+// boilerplate code -> templeate of java code
+public class JavaBasic {
+    public static void main(String args[]){
+        // print
+        // System.out.print("Hello World");
+
+        // println
+        System.out.println("Hello OM");
+        System.out.println("Become a programmer");
+
+        // "\n"
+        System.out.print("Hello World \n");
+        System.out.print("love \n Java");
+    }
+}
